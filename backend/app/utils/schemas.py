d09e1e7b6e2603cb3dd04dcff1f84052ff9cec53
POONA ABUBAKAR SIDDIQ <132919722+ABUBAKARSIDDIQPOONA786@users.noreply.github.com>
2026-01-02 20:36:09 +0530

    Create schemas.py

---
+++ backend/app/utils/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from typing import List
+
+class RoleRecommendation(BaseModel):
+    title: str
+    match_score: int
+
+class ATSDetails(BaseModel):
+    skills_detected: List[str]
+    sections_found: List[str]
+    sections_missing: List[str]
+    word_count: int
+
+class ATSAnalysis(BaseModel):
+    total_score: int
+    summary: str      # For AnalysisBox.jsx
+    explanation: str  # For ExplanationBox.jsx
+    details: ATSDetails
+
+class ResumeAnalysisResponse(BaseModel):
+    filename: str
+    status: str
+    ats_analysis: ATSAnalysis
+    recommended_roles: List[RoleRecommendation]
